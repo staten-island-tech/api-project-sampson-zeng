4cfab4ddabf020dc089c3051ec4168d0e0725619
SampsonZeng <71471145+SampsonZeng@users.noreply.github.com>
2021-05-13 23:37:27 -0400

    kjogdsjoersdhgsdg

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,36 @@ import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
 const key = "IeQRy8GwrRH2tC18ozyqgLhBwjwzQtO1C30FOcLH";
-// api key for food api//
+const query = async function () {
+  try {
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/random?complexSearch?addRecipeInformation=true&sort=healthiness&apiKey=1e36a4fa47e6498e8d1346724fa2ca66`
+    );
+    const data = await response.json();
+    data.results.forEach((recipes) => {
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `<div class="recipe-card">
+        <div class="recipe-card-front">
+        </div>
+        < class="recipe-card-back">
+          <h3 class="recipe-card-header">${recipes.original_name}</h3>
+          <div class="score-box">
+            <p class="health-score">Community Score</p>
+            <p class="health-score">${recipes.health_score}</p>
+          </div>
+          <div class="food-box">
+            <p class="food-form">Consistency</p>
+            <p class="food-form">${recipes.consistency}</p>
+          </div>
+        </div>
+      </div>`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+    alert("Hey something went wrong");
+  }
+};
+
+query();
