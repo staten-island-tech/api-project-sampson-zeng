475b98a704288421e5956e623ea9cbee3c791262
SampsonZeng <71471145+SampsonZeng@users.noreply.github.com>
2021-05-14 10:24:18 -0400

    qwertyuiop

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,11 @@ import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
 const key = "IeQRy8GwrRH2tC18ozyqgLhBwjwzQtO1C30FOcLH";
+
 const query = async function () {
   try {
     const response = await fetch(
-      `https://api.spoonacular.com/recipes/random?complexSearch?addRecipeInformation=true&sort=healthiness&apiKey=1e36a4fa47e6498e8d1346724fa2ca66`
+      `https://api.spoonacular.com/recipes/complexSearch?addRecipeInformation=true&apiKey=1e36a4fa47e6498e8d1346724fa2ca66`
     );
     const data = await response.json();
     data.results.forEach((recipes) => {
@@ -15,14 +16,14 @@ const query = async function () {
         <div class="recipe-card-front">
         </div>
         < class="recipe-card-back">
-          <h3 class="recipe-card-header">${recipes.original_name}</h3>
+          <h3 class="recipe-card-header">${recipes.title}</h3>
           <div class="score-box">
             <p class="health-score">Community Score</p>
             <p class="health-score">${recipes.health_score}</p>
           </div>
           <div class="food-box">
-            <p class="food-form">Consistency</p>
-            <p class="food-form">${recipes.consistency}</p>
+            <p class="spoon-score">Spoonacular Score</p>
+            <p class="spoon-score">${recipes.spoonacular_score}</p>
           </div>
         </div>
       </div>`
