82d813da7c9b743828afba01c2b7ee758db8f495
SampsonZeng <71471145+SampsonZeng@users.noreply.github.com>
2021-05-26 22:49:51 -0400

    Revert "sdgosdgkhjlf"
    This reverts commit 2cfbbaf4f79d613c7211e0daea8f28f1016bfced.

--- src/js/index.js
+++ src/js/index.js
@@ -19,11 +19,11 @@ const query = async function () {
           <h3 class="recipe-card-header">${recipes.title}</h3>
           <div class="score-box">
             <p class="health-score">Community Score</p>
-            <p class="health-score">${recipes.dairy_free}</p>
+            <p class="health-score">${recipes.health_score}</p>
           </div>
           <div class="food-box">
             <p class="spoon-score">Spoonacular Score</p>
-            <p class="spoon-score">${recipes.gluten_free}</p>
+            <p class="spoon-score">${recipes.spoonacular_score}</p>
           </div>
         </div>
       </div>`
