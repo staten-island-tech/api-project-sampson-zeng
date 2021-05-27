e601bd135365d12f19e0abb36a541042463243b9
SampsonZeng <71471145+SampsonZeng@users.noreply.github.com>
2021-05-26 22:36:03 -0400

    plz do something

--- src/js/index.js
+++ src/js/index.js
@@ -17,7 +17,7 @@ const query = async function () {
         </div>
         <div class="recipe-card-back">
           <h3 class="recipe-card-header">${recipes.title}</h3>
-          <div class="type-box">
+          <div class="score-box">
             <p class="dairy-free">Dairy Free</p>
             <p class="dairy-free">${recipes.dairy_free}</p>
           </div>
